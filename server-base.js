451c99472f9b4061ad03a2dcdb82dba97cd17fa7
Sam Dickinson <sam.dickinson@servictoria.com.au>
2024-10-03 16:26:35 +1000

    Remove vite from server-base

--- server-base.js
+++ server-base.js
@@ -4,26 +4,8 @@ import express from "express";
 import morgan from "morgan";
 
 (async () => {
-  const viteDevServer =
-    process.env.NODE_ENV === "production"
-      ? undefined
-      : await import("vite").then((vite) =>
-          vite.createServer({
-            build: {
-              rollupOptions: {
-                input: {
-                  server: "./server/index.js",
-                },
-              },
-            },
-            server: { middlewareMode: true },
-          })
-        );
-
   const remixHandler = createRequestHandler({
-    build: viteDevServer
-      ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
-      : await import("./server/index.js"),
+    build: await import("./server/index.js"),
   });
 
   const app = express();
@@ -34,15 +16,11 @@ import morgan from "morgan";
   app.disable("x-powered-by");
 
   // handle asset requests
-  if (viteDevServer) {
-    app.use(viteDevServer.middlewares);
-  } else {
-    // Vite fingerprints its assets so we can cache forever.
-    app.use(
-      "/assets",
-      express.static("./client/assets", { immutable: true, maxAge: "1y" })
-    );
-  }
+  // Vite fingerprints its assets so we can cache forever.
+  app.use(
+    "/assets",
+    express.static("./client/assets", { immutable: true, maxAge: "1y" })
+  );
 
   // Everything else (like favicon.ico) is cached for an hour. You may want to be
   // more aggressive with this caching.
