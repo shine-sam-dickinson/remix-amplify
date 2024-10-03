1136f3f824af97ff2fdb186414af051535b6fd23
Sam Dickinson <sam.dickinson@servictoria.com.au>
2024-10-03 13:20:09 +1000

    Bunder server

--- postbuild.sh
+++ postbuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-cp server.js .amplify-hosting/compute/default
-npm ci --omit dev
+cp server-base.js .amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r public ./.amplify-hosting/static
-cp package.json ./.amplify-hosting/compute/default/package.json
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+cp package.json ./.amplify-hosting/compute/default/package.json
+cd .amplify-hosting/compute/default
+node_modules/.bin/esbuild --bundle server-base.js --outfile=server.cjs --platform=node --external:pkg --external:lightningcss --external:vite
+rm -rf node_modules
