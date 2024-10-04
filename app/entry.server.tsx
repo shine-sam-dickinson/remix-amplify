20a49139dc899f6db1c1f85b7038a1405c4bb635
Sam Dickinson <sam.dickinson@servictoria.com.au>
2024-10-04 13:59:49 +1000

    Remove dotenv

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -11,9 +11,6 @@ import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
-import { config } from "dotenv";
-
-config();
 
 const ABORT_DELAY = 5_000;
 
