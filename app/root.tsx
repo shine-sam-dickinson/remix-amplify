c0485f0b55ca2b26ba9a55f97261e548882aa509
Sam Dickinson <sam.dickinson@servictoria.com.au>
2024-10-04 13:51:34 +1000

    dump why we're not tracking

--- app/root.tsx
+++ app/root.tsx
@@ -49,7 +49,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {process.env.NODE_ENV === "development" || !gaTrackingId ? 
-          <div dangerouslySetInnerHTML={{ __html: `<!-- No tracking -->` }} />
+          <div dangerouslySetInnerHTML={{ __html: `<!-- No tracking (${process.env.NODE_ENV}, ${gaTrackingId}) -->` }} />
          : (
           <>
             <script
